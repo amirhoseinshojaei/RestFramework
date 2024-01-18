0a3216d379a46abd6c1f0db3884cbc7300863f38
amirhosein <amirhoseinbu80@gmail.com>
2024-01-18 20:23:05 +0330

    register book models

--- rest/book/admin.py
+++ rest/book/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+
+class BookAdmin (admin.ModelAdmin):
+
+    list_display = [
+        'book_name',
+        'publish_at',
+        'user'
+    ]
+
+    list_filter = [
+        'user',
+        'publish_at'
+    ]
+
+    search_fields = ['book_name']
+
+    date_hierarchy = 'publish_at'
+
+admin.site.register (Book,BookAdmin)
