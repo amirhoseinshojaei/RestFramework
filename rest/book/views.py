cbf5544f4b1f53e60d5cdf2587157d81d9695288
amirhosein <amirhoseinbu80@gmail.com>
2024-01-18 23:50:48 +0330

    create listapi for books

--- rest/book/views.py
+++ rest/book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.generics import ListCreateAPIView
 # Create your views here.
+
+
+class BookListApi (ListCreateAPIView):
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
