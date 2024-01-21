a59d61b08bec3eae2f38429a846006d7e3f2a8ce
amirhosein <amirhoseinbu80@gmail.com>
2024-01-21 18:00:17 +0330

    serializer for writer

--- rest/book/serializers.py
+++ rest/book/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book , Writer
 from django.contrib.auth.models import User
 
+
+
+class WriterSerializer (serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Writer
+
+        fields = [
+
+            'first_name',
+            'last_name',
+            'writer',
+        ]
 
 class UserSerializer (serializers.ModelSerializer):
 
@@ -10,6 +24,8 @@ class UserSerializer (serializers.ModelSerializer):
         fields = ('username',)
 
 class BookSerializer (serializers.ModelSerializer):
+    
+    writer = WriterSerializer (many = True)
     
     user = UserSerializer (many = False)
     class Meta:
