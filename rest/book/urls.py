356f6e40027292f88ca47a41530e8d1eb22f46ce
amirhosein <amirhoseinbu80@gmail.com>
2024-01-27 10:56:21 +0330

    create path for bookdetail

--- rest/book/urls.py
+++ rest/book/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path ('',views.BookListApi.as_view(),name= 'book_list'),
+
+    path ('<int:pk>/' , views.BookDetialApi.as_view(), name= 'book-detail'),
 ]
