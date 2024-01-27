065ff11399e5eee780b20c731b255db1b31699d3
amirhosein <amirhoseinbu80@gmail.com>
2024-01-27 10:55:43 +0330

    create view for detail

--- rest/book/views.py
+++ rest/book/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPIView
 # Create your views here.
 
 
 class BookListApi (ListCreateAPIView):
 
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetialApi (RetrieveUpdateDestroyAPIView):
+
+    queryset = Book.objects.all ()
+
     serializer_class = BookSerializer
