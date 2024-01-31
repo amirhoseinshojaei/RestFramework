18f79b0d8f33e39f16cb91c697a95d33883b707a
amirhosein <amirhoseinbu80@gmail.com>
2024-01-31 16:18:59 +0330

    create view for user

--- rest/user/views.py
+++ rest/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
+from .serializers import UserSerialzer
 # Create your views here.
+
+class UserViewSet (viewsets.ModelViewSet):
+
+    queryset = User.objects.all()
+    permission_classes = (IsAdminUser,)
+    serializer_class = UserSerialzer
